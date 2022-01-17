a68d72ae9fcda1a702d65f1c7b9736eb57b86aa9
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2022-01-17 20:07:12 +0300

    Initial commit

---
+++ TURKISH.md
@@ -0,0 +1,107 @@
+# DEĞİŞİKLİKLER:
+
+Client intentlerini elle girin.
+
+```js
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES] });
+```
+
+SADECE embed değişkeniyle tanımlanmış embedleri değiştirebilir.
+
+
+# ELLE YAPILMALI: (Program nerede olduğunu gösterecek.)
+
+
+message => messageCreate
+
+```js
+const collector = message.createReactionCollector(filter, { time: 15000 });
+const collector = message.createReactionCollector({ filter, time: 15000 });
+
+ const reactions = await message.awaitReactions(filter, { time: 15000 });
+const reactions = await message.awaitReactions({ filter, time: 15000 });
+```
+
+```js
+let count = 5;
+embed.addField('Count', count);
+embed.addField('Count', count.toString())
+```
+
+```js
+permissions.has('SEND_MESSAGES')
+ permissions.has(Permissions.FLAGS.SEND_MESSAGES) 
+
+```
+### Bunları
+
+- news - a guild news channel
+- store - a guild store channel
+- unknown - a generic channel of unknown type, could be Channel or GuildChannel
+### Böyle yapın hacılar
+- GUILD_NEWS - a guild news channel
+- GUILD_STORE - a guild store channel
+- UNKNOWN - a generic channel of unknown type, could be Channel or GuildChannel
+
+```js
+ client.user.setPresence({ activity: { name: 'with discord.js' } });
+ client.user.setPresence({ activities: [{ name: 'with discord.js' }] });
+```
+```js
+ Guild.fetchVanityCode().then(code => console.log(`Vanity URL: https://discord.gg/${code}`));
+ Guild.fetchVanityData().then(res => console.log(`Vanity URL: https://discord.gg/${res.code} with ${res.uses} uses`));
+
+```
+
+#### Guild owner 
+```js
+ console.log(guild.owner);
+ guild.fetchOwner().then(console.log);
+```
+```js
+guild.voice
+ guild.me.voice
+```
+
+```js
+member.ban('reason')
+ member.ban({ reason: 'reason' })
+
+```
+
+
+```js
+message.delete({ timeout: 10000 });
+setTimeout(() => message.delete(), 10000);
+
+```
+
+```js
+
+ channel.send({ embeds: [new MessageEmbed().setTitle("Files").attachFiles(file)] })
+ channel.send({ embeds: [new MessageEmbed().setTitle("Files")], files: [file] })
+```
+
+```js
+
+ guild.roles.create({ data: { name: "New role" } }, "Creating new role");
+ guild.roles.create({ name: "New role", reason: "Creating new role" })
+```
+
+#### Shard
+```js
+ shard.respawn(500, 30000);
+ shard.respawn({ delay: 500, timeout: 30000 });
+```
+
+Presencelere User altından erişemezsiniz. Sadece Member altından erişiliyor.
+
+
+#### Webhook
+```js
+- new WebhookClient(id, token, options);
++ new WebhookClient({ id, token }, options);
+
++ new WebhookClient({ url }, options);
+
+```
---
+++ index.js
@@ -0,0 +1,119 @@
+const fs = require("fs")
+
+fs.readdirSync('./commands').forEach(dir => {
+    const commandFiles = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) { 
+
+        var data = fs.readFileSync(`./commands/${dir}/${file}`, "utf-8");
+
+
+
+        if (!data.includes("const embed") && data.includes("MessageEmbed()")) {
+            console.log("Embed Problem!   ", dir + "/" + file)
+        }
+
+        if (data.includes(".createReactionCollector")) {
+            console.log("createReactionCollector   ", dir + "/" + file)
+        }
+
+        if (data.includes(".awaitReactions")) {
+            console.log("createReactionCollector   ", dir + "/" + file)
+        }
+
+        if (data.includes(".awaitMessages")) {
+            console.log(".awaitMessages   ", dir + "/" + file)
+        }
+
+        //WARN FOR ADDFIELDS!
+        if (data.includes("addField")) {
+            //   console.log("Embed warn! (To string)   ", dir+"/"+file)
+        }
+
+
+        //CAN FETCH BOT INVITE LINK 
+        if (data.includes("permissions")) {
+            console.log("Permisson  ", dir + "/" + file)
+        }
+
+        if (data.includes("unknown")) {
+            console.log("unknown   ", dir + "/" + file)
+        }
+
+        if (data.includes("setPresence")) {
+            console.log("setPresence   ", dir + "/" + file)
+        }
+
+        if (data.includes("fetchVanityCode")) {
+            console.log("fetchVanityCode   ", dir + "/" + file)
+        }
+        if (data.includes(".owner")) {
+            console.log("guild.owner   ", dir + "/" + file)
+        }
+
+
+        if (data.includes(" .voice")) {
+            console.log("guild.voice   ", dir + "/" + file)
+        }
+        if (data.includes(".ban")) {
+            console.log("ban   ", dir + "/" + file)
+        }
+        if (data.includes(".kick")) {
+            console.log("kick   ", dir + "/" + file)
+        }
+
+
+
+
+        if (data.includes(".delete")) {
+            console.log("Check delete() .   ", dir + "/" + file)
+        }
+        if (data.includes("roles.create")) {
+            console.log("roles.create   ", dir + "/" + file)
+        }
+        if (data.includes("WebhookClient")) {
+            console.log("WebhookClient  ", dir + "/" + file)
+        }
+        if (data.includes("stopTyping")) {
+            console.log("stopTyping  ", dir + "/" + file)
+        }
+
+
+
+        const newData = data
+            .replace(/send\(embed\)/g, "send({embeds: [embed]})")
+            .replace(/reply\(embed\)/g, "reply({embeds: [embed]})")
+
+
+            .replace(/.ownerID/g, ".ownerId")
+            .replace(/.channelID/g, ".channelId")
+            .replace(/CUSTOM_STATUS/g, "CUSTOM")
+            .replace(/text/g, `GUILD_TEXT`)
+            .replace(/dm/g, `DM`)
+            .replace(/category/g, `GUILD_CATEGORY`)
+            // .replace(/message/g, `messageCreate`)//IMPORTANT
+            .replace(/.fetchBans/g, `.bans.fetch`)
+            .replace(/.fetchInvites/g, `.invites.fetch`)
+            .replace(/.member\(/g, `.members.cache.get(`)
+            .replace(/.overwritePermissions\(/g, `.permissionOverwrites.set(`)
+
+
+            .replace(/.updateOverwrite/g, `.permissionOverwrites.edit`)
+            .replace(/.hasPermission/g, `.permissions.has`)
+            .replace(/.createOverwrite/g, `.permissionOverwrites.create`)
+
+            .replace(/MANAGE_EMOJIS/g, `MANAGE_EMOJIS_AND_STICKERS`)
+            .replace(/DISCORD_PARTNER/g, `PARTNERED_SERVER_OWNER`)
+            .replace(/VERIFIED_DEVELOPER/g, `EARLY_VERIFIED_BOT_DEVELOPER`)
+
+
+
+            .replace(/.addMember/g, `.members.add`)
+
+        fs.writeFileSync(`./commandsV13/${dir}/${file}`, newData)
+
+    }
+})
+
+
+
